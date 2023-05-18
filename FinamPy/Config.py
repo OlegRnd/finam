0f7733e5283e7157b64a83b465cd5833432c103f
OlegRnd <scriptmake@gmail.com>
2023-05-18 22:45:47 +0300

    Added FinamPy grpc

---
+++ FinamPy/Config.py
@@ -0,0 +1,8 @@
+# Чтобы получить Refresh Token:
+# 1. Открыть счет в "Финаме" https://open.finam.ru/registration
+# 2. Зарегистрироваться в сервисе Comon https://www.comon.ru/
+# 3. В личном кабинете Comon получить токен https://www.comon.ru/my/trade-api/tokens
+
+class Config:
+    ClientIds = ('757282RDVBT',)  # Торговые счёта
+    AccessToken = 'CAEQ1ZACGhjrE64no9apCjdI6k+dMfQso3wM6TKzq9U='  # Торговый токен доступа
--- FinamPy/Examples/03 - Ticker.py
+++ FinamPy/Examples/03 - Ticker.py
@@ -1,21 +1,3 @@
-# import time
-# # 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
-# token = "CAEQ1ZACGhjrE64no9apCjdI6k+dMfQso3wM6TKzq9U="
-# cid = "757282RDVBT"
-
-# from FinamRestPy import *
-
-# fr = FinamRestPy(client_id=cid, access_token=token)
-# res = fr.get_securities()
-
-# # print(res['securities'])
-# # quit()
-
-# for i in res['securities']:
-#     if (i['code'].lower() == 'sim3'):
-#         print(i)
-
-
 from FinamPy.FinamPy import FinamPy
 from FinamPy.Config import Config
 
@@ -26,20 +8,6 @@ if __name__ == '__main__':  # Точка входа при запуске это
     symbols = (('TQBR', 'SBER'), ('FUT', 'SiM3'), ('FUT', 'RIM3'))  # Кортеж тикеров в виде (код площадки, код тикера)
 
     print('Получаем информацию обо всех тикерах (займет несколько секунд)...')
-    
-    res = fp_provider.subscribe_order_book('SiM3','FUT')
-
-    # Проверяем работу подписок
-    print(f'\nПодписка на стакан тикера: FUT.SiM3')
-    print('ask - минимальная цена покупки, bid - максимальная цена продажи')
-    fp_provider.on_order_book = lambda order_book: print('ask:', order_book.asks[0].price, 'bid:', order_book.bids[0].price)  # Обработчик события прихода подписки на стакан
-    # fp_provider.subscribe_order_book(security_code, security_board, 'orderbook1')  # Подписываемся на стакан тикера   
-
-    input('Enter - выход\n')
-    res = fp_provider.unsubscribe_order_book(res,'SiM3','FUT')
-    fp_provider.close_channel()
-    quit()
-
     securities = fp_provider.get_securities()  # Получаем информацию обо всех тикерах
     # print('Ответ от сервера:', securities)
     for board, symbol in symbols:  # Пробегаемся по всем тикерам
